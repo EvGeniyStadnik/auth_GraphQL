cc57492bac867e7fb069f1f005f16d147612844e
EvGeniyStadnik <evgeniy.stadnik@gmail.com>
2018-01-14 15:04:50 +0200

    git init commit

---
+++ server/schema/mutations.js
@@ -0,0 +1,28 @@
+const graphql = require('graphql');
+
+const {
+  GraphQLString,
+  GraphQLObjectType,
+} = graphql;
+
+const UserType = require('./types/user_type');
+const AuthService = require('../services/auth');
+
+const mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    signup: {
+      type: UserType,
+      args: {
+        email: { type: GraphQLString },
+        password: { type: GraphQLString },
+      },
+      resolve(parentValue, { email, password }, request){
+        return AuthService.signup({ email, password, request })
+      }
+    }
+
+  }
+});
+
+module.exports = mutation;
